7b20bab8c63b71cff7564c2031df1291b7197196
Prashant joshi <prashantjoshi20428@gmail.com>
2025-03-04 05:47:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const acccountid = "1542"
+let accountemail = "hututu@gmail"
+var accountpassword = 2131
+accountcity = "delhi"
+
+/*accountId = 2 (not allowed)
+
+accountemail = "ahah@f.com"
+accountpassword = "2314"
+accountcity = "delhi" 
+console.log(accountId);
+
+/* prefer not to use var
+because of issue in block scope and functional
+console.table([accountId, accountemail, accountpassword, accountcity, accountstate])*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+alert(1 + 2)
+
+ // number => 2 to the power 53
+ // bigint (for big nums)
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined => 
+ // symbol => unique
+
+
+ // object
+
+ console.log(typeof null);
