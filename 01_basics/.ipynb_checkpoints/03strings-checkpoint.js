f0125a9adc0984b171a8e72c1313a094fd04a409
Prashant joshi <prashantjoshi20428@gmail.com>
2025-03-05 17:22:21 +0000

    strings done

---
+++ 01_basics/.ipynb_checkpoints/03strings-checkpoint.js
@@ -0,0 +1,11 @@
+const newStringOne = "prashant"
+console.log(newStringOne)
+console.log(newStringOne.trim());
+
+const url= "https://prashant.com/prashant%20joshi"
+
+console.log(url.repalce('%20','-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
---
+++ 01_basics/03strings.js
@@ -0,0 +1,16 @@
+const name = "prashant"
+
+const gameName = new String ("prashant-pj-com");
+
+const newStringOne = "prashant"
+
+// console.log(newStringOne)
+// console.log(newStringOne.trim());
+
+const url= "https://prashant.com/prashant%20joshi"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('joshi'))
+
+console.log(gameName.split('-'));
